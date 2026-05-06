11ec319774cc6111cea819b983fc3e2c5a8c6327
nadznyu <nadiaalya0107@gmail.com>
2026-05-06 11:15:29 +0700

    class node

---
+++ Pertemuan12/NodeMahasiswa16.java
@@ -0,0 +1,11 @@
+package Pertemuan12;
+
+public class NodeMahasiswa16 {
+    Mahasiswa16 data;
+    NodeMahasiswa16 next;
+
+    public NodeMahasiswa16(Mahasiswa16 data, NodeMahasiswa16 next) {
+        this.data = data;
+        this.next = next;
+    }
+}
